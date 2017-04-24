714dc21efb3117a3f8ebc70d5b7290495135dc60
Laurensmarnixkalf <laurens@sense-os.nl>
2017-04-24 20:48:16 +0200

    Create lmk.js

---
+++ js/lmk.js
@@ -0,0 +1,20 @@
+var jumboTitle = document.getElementById("headline");
+
+var findTitle = document.getElementsByTagName("h4");
+
+for (i = 0; i < findTitle.length; i++) {
+  findTitle[i].addEventListener("click", changeTitle);
+}
+
+function changeTitle() {
+  jumboTitle.innerHTML = this.innerHTML;
+
+  console.log (jumboTitle.textContent);
+}
+
+console.log("Proof of concept")
+console.log("doet ie t nou?")
+
+/*
+.getElementById("banner")
+.getElementById('topics')*/
