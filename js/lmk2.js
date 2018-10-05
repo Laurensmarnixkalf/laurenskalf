b32118c52ba8e6314fca86a388df3be951783cbf
Laurens Kalf <laurenskalf@Sattes-iMac.local>
2018-10-05 13:48:27 +0200

    Added a new 'Books' section

--- js/lmk2.js
+++ js/lmk2.js
@@ -54,14 +54,13 @@ var getAllBars = document.getElementById("skillsContainer").querySelectorAll(".p
 // Turn Nodelist into array
 var arrayOfBars = Array.prototype.slice.call(getAllBars);
 // Create array containing values for bars
-var valueArray = ['70%', '75%', '65%', '65%', '40%', '80%', '55%', '15%'];
+var valueArray = ['80%', '75%', '65%', '60%', '40%', '80%', '55%', '15%'];
 // Function that fills all bars with right values
 var barFiller = function () {
     for (i=0; i<arrayOfBars.length; i++) {
         arrayOfBars[i].setAttribute('style', `width: ${valueArray[i]}`);
     }
 };
-
 
 
 
